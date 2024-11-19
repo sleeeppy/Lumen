a3d830ee6c167cab6d69a4e37fb41208015ae768
yujun07 <ujun08140@naver.com>
2024-11-19 14:35:19 +0900

    Update
    Boss2

---
+++ Assets/Scripts/Boss2.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using Random = UnityEngine.Random;
+
+public class Boss2 : Boss
+{
+    private GameObject player;
+    private Vector3 playerPos;
+    private float randomY;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        player = GameObject.FindWithTag("Player");
+        //Debug.Log($"{bottomBorder + 4.5}, {topBorder - 3}");
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        playerPos = player.transform.position;
+
+        if(HP == 60f || HP == 30f)
+        {
+            float randomX = Mathf.Clamp(Random.Range(transform.position.x - 10, transform.position.x + 10), leftBorder + 10, rightBorder - 10);
+            if (phase != 4)
+                randomY = Mathf.Clamp(Random.Range(bottomBorder + 4.5f, topBorder - 3), bottomBorder + 4.5f, topBorder - 3);
+            else
+                randomY = Mathf.Clamp(Random.Range(bottomBorder + 4.5f, topBorder - 3), bottomBorder + 4.5f, topBorder - 3);
+            transform.DOMove(new Vector3(randomX, randomY, transform.position.z), 3f).SetEase(Ease.InOutBack);
+        }
+    }
+}
---
+++ Assets/Scripts/ParticlePool.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class ParticlePool : MonoBehaviour
+{
+    public static ParticlePool Instance { get; private set; }
+    [AssetsOnly]
+    [SerializeField] private GameObject[] particlePrefabs;
+    [SerializeField] private int poolSize = 10;
+
+    private List<Queue<GameObject>> particlePools;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        particlePools = new List<Queue<GameObject>>();
+
+        for (int i = 0; i < particlePrefabs.Length; i++)
+        {
+            InitializePool(i);
+        }
+    }
+    //초기화
+    private void InitializePool(int typeIndex)
+    {
+        Queue<GameObject> pool = new Queue<GameObject>();
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject particle = Instantiate(particlePrefabs[typeIndex]);
+            particle.SetActive(false);
+            pool.Enqueue(particle);
+        }
+
+        particlePools.Add(pool);
+    }
+    //사용
+    public GameObject GetParticle(int typeIndex)
+    {
+        if (particlePools[typeIndex].Count > 0)
+        {
+            GameObject particle = particlePools[typeIndex].Dequeue();
+            particle.SetActive(true);
+            return particle;
+        }
+        else
+        {
+            return Instantiate(particlePrefabs[typeIndex]);
+        }
+    }
+    //반환
+    public void ReturnParticle(int typeIndex, GameObject particle)
+    {
+        particle.SetActive(false);
+        particlePools[typeIndex].Enqueue(particle);
+    }
+}

6015199d6ccc91ea05e68c515e9d288106f959ce
yujun07 <ujun08140@naver.com>
2024-12-30 19:58:45 +0900

    Update
    beam destructible

--- Assets/Scripts/Projectile/DestructibleProjectile.cs
+++ Assets/Scripts/Projectile/DestructibleProjectile.cs
@@ -7,16 +7,30 @@ using UnityEngine.UIElements;
 
 public class DestructibleProjectile : MonoBehaviour
 {
+    [SerializeField] private float destructionTime = 0.2f;
+    private float hitTimer = 0f;
+    private bool isHit = false;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PlayerBullet"))
         {
-            RetroProjectileScript projectileScript = GetComponent<RetroProjectileScript>();
-            GameObject impactP = Instantiate(projectileScript.impactParticle, transform.position, Quaternion.FromToRotation(Vector3.up, other.transform.position.normalized)) as GameObject;
-            Destroy(impactP, 5.0f);
-            Destroy(projectileScript.projectileParticle, 3f);
-            projectileScript.DestroyMissile();
-            Destroy(other.gameObject);
+            DestroyProjectile(other.gameObject);
         }
+    }
+    private void DestroyProjectile(GameObject gameObject)
+    {
+        RetroProjectileScript projectileScript = GetComponent<RetroProjectileScript>();
+        GameObject impactP = Instantiate(projectileScript.impactParticle, transform.position, Quaternion.FromToRotation(Vector3.up, gameObject.transform.position.normalized)) as GameObject;
+        Destroy(impactP, 5.0f);
+        Destroy(projectileScript.projectileParticle, 3f);
+        projectileScript.DestroyMissile();
+        Destroy(gameObject);
+    }
+    public void OnHit(float deltaTime)
+    {
+        isHit = true;
+        hitTimer += deltaTime;
+
+        if (hitTimer >= destructionTime) DestroyProjectile(this.gameObject);
     }
 }
